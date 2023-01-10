7a668bf2cbd45361556cf04316c785262593d84e
MUHIB-143 <122245772+MUHIB-143@users.noreply.github.com>
2023-01-10 01:45:11 -0800

    mhd_logo.py

--- Logo.py
+++ Logo.py
@@ -1,65 +1,18 @@
-stem("git pull")
-from bs4 import BeautifulSoup as sop
-from bs4 import BeautifulSoup
-import requests as ress
-from datetime import date
-from datetime import datetime
-from time import sleep
-from time import sleep as waktu
-try:
-    import requests
-    from concurrent.futures import ThreadPoolExecutor as ThreadPool
-    import mechanize
-    from requests.exceptions import ConnectionError
-except ModuleNotFoundError:
-    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
-    os.system('pip install bs4')
-    
-
-            
-            
-
-class jalan:
-    def __init__(self, z):
-        for e in z + "\n":
-            sys.stdout.write(e)
-            sys.stdout.flush()
-            time.sleep(0.009)
-            
-P = '\x1b[1;97m'
-M = '\x1b[1;91m'
-H = '\x1b[1;92m'
-K = '\x1b[1;93m'
-B = '\x1b[1;94m'
-U = '\x1b[1;95m' 
-O = '\x1b[1;96m'
-N = '\x1b[0m'    
-Z = "\033[1;30m"
-sir = '\033[41m\x1b[1;97m'
-x = '\33[m' # DEFAULT
-m = '\x1b[1;91m' #RED +
-k = '\033[93m' # KUNING +
-xr = '\x1b[1;92m' # HIJAU +
-hh = '\033[32m' # HIJAU -
-u = '\033[95m' # UNGU
-kk = '\033[33m' # KUNING -
-b = '\33[1;96m' # BIRU -
-p = '\x1b[0;34m' # BIRU +
-asu = random.choice([m,k,xr,u,b])
-my_color = [
- P, M, H, K, B, U, O, N]
-warna = random.choice(my_color)
-now = datetime.now()
-dt_string = now.strftime("%H:%M")
-current = datetime.now()
-ta = current.year
-bu = current.month
-ha = current.day
-today = date.today()
-os.system('xdg-open https://www.facebom/its.muhib.7?ref=share_group_link')
+os.system('xdg-open https://www.facebook.com/its.muhib.7?ref=share_group_link')
 logo =("""\033[0;92m
-[1;92m███    ███ [1;94m ██      ██ [1;93m ██   ██ [1;91m ██ [1;95m██████  
-[1;92m████  ████[1;94m ██      ██[1;93m ██   ██[1;91m ██[1;95m ██         ██
-[1;92m██ ████ ██[1;94m ██      ██[1;93m ███████[1;91m ██[1;95m ██████
-[1;92m██  ██  ██[1;94m ██      ██[1;93m ██   ██[1;91m ██[1;95m ██         ██
-[1;92m██      ██[1;94m    ████[1;93m ██   ██[1;91m ██[1;95m ██████
+   [1;92m███    ███[1;94m ██      ██[1;93m ██   ██[1;91m ██[1;95m ██████ 
+   [1;92m████  ████[1;94m ██      ██[1;93m ██   ██[1;91m ██[1;95m ██   ██
+   [1;92m██ ████ ██[1;94m ██      ██[1;93m ███████[1;91m ██[1;95m ██████
+   [1;92m██  ██  ██[1;94m ██      ██[1;93m ██   ██[1;91m ██[1;95m ██   ██
+   [1;92m██      ██[1;94m ██████████[1;93m ██   ██[1;91m ██[1;95m ██████
+[1;92m╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗[1;92m
+[1;92m║  [1;97m[1;41m    FREE  NUMBER CLONING V1     [1;0m[1;92m║[1;92m
+[1;92m╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝[1;92m
+[1;92m╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗[1;92m
+[1;92m║➣ [1;91mDEVOLPER   :          MUHIB-143 [1;92m          ║
+[1;92m║➣ [1;92mFACEBOOK   :          MUHIB-143 [1;92m          ║
+[1;92m║➣ [1;93mWHATSAPP   :          01405744958 [1;92m        ║
+[1;92m║➣ [1;94mGITHUB     :          MUHIB-143[1;92m           ║
+[1;92m║➣ [1;95mTOOLS      :           NUMBER CRACKING[1;92m    ║
+[1;92m╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝
+    """) 
